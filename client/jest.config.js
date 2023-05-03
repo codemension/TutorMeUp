eb24b86590c2897ac7462f8cc80eeb1f246a93aa
codemension <codemension@gmail.com>
2023-05-03 14:46:03 +0500

    linted & formatted

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: ['/node_modules/'],
-}
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.{js,jsx}'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	transformIgnorePatterns: ['/node_modules/'],
+};
--- client/src/__test__/App.test.jsx
+++ client/src/__test__/App.test.jsx
@@ -1,12 +1,11 @@
-import {render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 
 describe('App tests', () => {
-  test('renders App component', () => {
-    render(<App />);
-    screen.debug();
-    expect(screen.getByText('Tutor Me Up Website')).toBeInTheDocument();
-  });
+	test('renders App component', () => {
+		render(<App />);
+		screen.debug();
+		expect(screen.getByText('Tutor Me Up Website')).toBeInTheDocument();
+	});
 });
-
